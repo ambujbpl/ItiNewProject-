e53c15411160d92ee6f2453c270c2609044b38ac
ambujbpl <ambujdubey89@gmail.com>
2018-02-05 08:40:06 +0530

    Nodejs RESTful API

---
+++ api/config/config.js
@@ -0,0 +1,22 @@
+module.exports={
+  "service": "gmail",
+  "mongodb": {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "admin_itijobs",
+    "multipleStatements": true
+  },
+  "authentication": {
+    "type": "OAuth2",
+    "user": "itijobs@gmail.com",
+    "clientId": "35023226250-h5fvkq3fll21ps.apps.googleusercontent.com",
+    "clientSecret": "xpVCzIMpqk-Lol",
+    "refreshToken": "1/7Kw6iBItOJHi3I0H4D2w",
+    "accessToken": "ya29.GlsYBXj8QlBbco_ofBnnnsTDFSEsF3EpFR8V9qmpk6Mb0K",
+  },
+  "maillist" :{
+    "admin1":"itijobs@gmail.com",
+    "admin2":"amitbpl@gmail.com",
+  }
+};
